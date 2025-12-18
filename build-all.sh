4eaec02b04d30384821f21c5cbdd511f6af8752f
redman13 <godslayerakp@gmail.com>
2025-12-17 19:23:28 -0800

    add auto config for glib and gdk-pixbuf

--- build-all.sh
+++ build-all.sh
@@ -1,2 +1,2 @@
-g++ ./main.cpp -I/usr/include/gdk-pixbuf-2.0 -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/x86-64/play `pkg-config --cflags --libs glib-2.0`
+g++ ./main.cpp -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/x86-64/play `pkg-config --cflags --libs glib-2.0` `pkg-config --cflags --libs gdk-pixbuf-2.0`
 ./build-local.sh;
--- build-local.sh
+++ build-local.sh
@@ -1,1 +1,1 @@
-g++ ./main.cpp -I/usr/include/gdk-pixbuf-2.0 -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/play -g `pkg-config --cflags --libs glib-2.0`
+g++ ./main.cpp -I/usr/include/gdk-pixbuf-2.0 -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/play -g `pkg-config --cflags --libs glib-2.0` `pkg-config --cflags --libs gdk-pixbuf-2.0`
