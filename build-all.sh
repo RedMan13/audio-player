2a904717cc6ef6dba3d6c983c7781a284400a088
redman13 <godslayerakp@gmail.com>
2025-12-17 19:17:28 -0800

    skibidi bop bop yes yes

--- build-all.sh
+++ build-all.sh
@@ -1,2 +1,2 @@
-g++ ./main.cpp -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/gdk-pixbuf-2.0 -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/x86-64/play
+g++ ./main.cpp -I/usr/include/gdk-pixbuf-2.0 -lao -lsndfile -lncurses -lX11 -lXext -lfontconfig -lXft -lnotify -lglib-2.0 -o ./dist/x86-64/play `pkg-config --cflags --libs glib-2.0`
 ./build-local.sh;
